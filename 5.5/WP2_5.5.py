8d26af8d778575f202df978d2a7f495f6064c78f
HugoArnalBernardino <harnalbernardi@tudelft.nl>
2025-12-10 16:59:27 +0100

    Previous Forces, Dimensions

--- 5.5/WP2_5.5.py
+++ 5.5/WP2_5.5.py
@@ -2,5 +2,14 @@ import numpy as np
 import math
 
 #Define constants 
-print ("hello world")
-print("FUCK")
+#PREVIOUS BACKPLATE DATA
+
+#    PREVIOUS FORCES
+
+F_x = 104.54 #N "left"
+F_z = 36.65  #N "up" 
+
+#  PREVIOUS DIMENSIONS
+W = 15.2 #mm
+D = 2 #mm
+H = 36 #mm
