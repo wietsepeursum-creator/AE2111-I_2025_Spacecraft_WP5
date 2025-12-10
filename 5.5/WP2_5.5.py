147bc39f75af0b7bee853854a2c85bcb729c62a4
wietsepeursum-creator <wietsepeursum@gmail.com>
2025-12-10 16:19:53 +0000

    New forces

--- 5.5/WP2_5.5.py
+++ 5.5/WP2_5.5.py
@@ -6,9 +6,9 @@ def new_dimensions(a_xtotal, a_ytotal, ):
     #Define constants 
     #PREVIOUS BACKPLATE DATA
     #    PREVIOUS FORCES
-    F_x = 104.54 #N "left"
-    F_z = 36.65  #N "up" 
-    F_z = 95.98  #N "in"
+    F_x = 209.07 #N "left"
+    F_z = 73.3  #N "up" 
+    F_y = 195.98  #N "out"
     #  PREVIOUS DIMENSIONS
     W = 15.2 #mm
     D = 2 #mm
