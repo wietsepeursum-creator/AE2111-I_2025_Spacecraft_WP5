c110600fbf63f41f8671fe50647582b60d7d1832
HugoArnalBernardino <harnalbernardi@tudelft.nl>
2025-12-10 17:07:47 +0100

    Made some changes can you see them

--- 5.5/WP2_5.5.py
+++ 5.5/WP2_5.5.py
@@ -1,15 +1,17 @@
 import numpy as np
 import math
 
+
+
+
+def new_dimensions(a_xtotal, a_ytotal, )
 #Define constants 
 #PREVIOUS BACKPLATE DATA
-
 #    PREVIOUS FORCES
-
 F_x = 104.54 #N "left"
 F_z = 36.65  #N "up" 
-
 #  PREVIOUS DIMENSIONS
 W = 15.2 #mm
 D = 2 #mm
 H = 36 #mm
+T = 3 #mm
