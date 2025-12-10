0675bae097011c892fbe1524a2b58209a73bcfae
wietsepeursum-creator <wietsepeursum@gmail.com>
2025-12-10 15:37:05 +0000

    test

--- 5.5/WP2_5.5.py
+++ 5.5/WP2_5.5.py
@@ -4,13 +4,3 @@ import math
 def resultant_force(forces):
     res = math.sqrt(forces[0]**2 + forces[1]**2 + forces[2]**2)
     return res
-
-def angle_between_forces(force1, force2):
-    dot_product = np.dot(force1, force2)
-    magnitude1 = np.linalg.norm(force1)
-    magnitude2 = np.linalg.norm(force2)
-    cos_theta = dot_product / (magnitude1 * magnitude2)
-    angle_rad = math.acos(cos_theta)
-    angle_deg = math.degrees(angle_rad)
-    return angle_deg
-
