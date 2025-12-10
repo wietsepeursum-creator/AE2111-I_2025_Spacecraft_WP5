14229c81ccb12ae464b2f0bdfcda88441ace5522
wietsepeursum-creator <wietsepeursum@gmail.com>
2025-12-10 15:56:03 +0000

    merge test

--- 5.5/WP2_5.5.py
+++ 5.5/WP2_5.5.py
@@ -6,12 +6,4 @@ import math
 
 def new_dimensions(a_xtotal, a_ytotal, )
 #Define constants 
-#PREVIOUS BACKPLATE DATA
-#    PREVIOUS FORCES
-F_x = 104.54 #N "left"
-F_z = 36.65  #N "up" 
-#  PREVIOUS DIMENSIONS
-W = 15.2 #mm
-D = 2 #mm
-H = 36 #mm
-T = 3 #mm
+print ("hello world")
