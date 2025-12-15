828bbe9a935c2ecc0dd9c761adb30520f172e801
Hugo Arnal Bernardino <harnalbernardi@tudelft.nl>
2025-12-15 13:27:40 +0000

    deleta accelerations

--- 5.5/WP2_5.5.py
+++ 5.5/WP2_5.5.py
@@ -1,11 +1,6 @@
 import numpy as np
 import math
 
-a_xtotal = 
-a_ytotal = 
-a_ztotal = 
-
-
 def new_dimensions(a_xtotal, a_ytotal, a_ztotal, n_fasteners, list_mass):
                 #Define constants
     #PREVIOUS BACKPLATE DATA
